b813b8ffb1979ef2e323dd978e11ad2e1c1bf50e
Viktor Mitrev <viktorim@abv.bg>
2018-03-30 18:06:14 +0300

    Problem 1&2

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,30 @@
+# Problem 1
+#A)
+x <- c(4,1,1,4)
+
+#B)
+y <- c(1,4)
+
+#C)
+x-y
+# the result is 3 -3 0 0 as R recycles the second vector as much times as neeeded
+# to "fill" the first one - in this case twice.
+
+#D)
+s <- c(x,y)
+
+#E)
+rep(s, 10)
+length(rep(s,10))
+
+#F)
+rep(s, each=3)
+
+#G)
+seq(7,21)
+7:21
+
+#H)
+length(seq(7,21))
+
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,40 @@
+#Problem 2
+#A)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+#B)
+TemepratureDifference <- xmax - xmin 
+
+#C)
+mean(xmin)
+mean(xmax)
+
+#D)
+xmin[xmin<mean(xmin)]
+
+#E)
+xmax[xmax>mean(xmax)]
+
+#F)
+DateNames <- c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+
+names(xmin) <- DateNames
+names(xmax) <- DateNames
+
+#G)
+temperatures <- data.frame(xmin,xmax)
+
+#H)
+temperatures <- within(temperatures, { xminFahrenheit <- xmin*9/5+32})
+temperatures
+
+#I)
+xminFahrenheit <- xmin*9/5+32
+xmaxFahrenheit <- xmax*9/5+32
+
+TemperaturesFahr <- data.frame(xminFahrenheit, xmaxFahrenheit)
+
+#J)
+NewDataFrame1 <-data.frame(TemperaturesFahr[c(1,2,3,4,5),])
 
